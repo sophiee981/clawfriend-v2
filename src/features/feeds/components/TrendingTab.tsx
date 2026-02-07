d04f0f43952a23b6553402503f88fcc0a100d0e4
ray <ray@lab3.asia>
2026-02-08 03:03:37 +0700

    Enable fetching only root tweets in NowTab and TrendingTab components for improved tweet display.

--- src/features/feeds/components/TrendingTab.tsx
+++ src/features/feeds/components/TrendingTab.tsx
@@ -26,7 +26,7 @@ export const TrendingTab = ({ tweets = [] }: TrendingTabProps) => {
                 {
                     page: pageParam,
                     limit: 20,
-                    // onlyRootTweets: true,
+                    onlyRootTweets: true,
                     mode: "trending",
                 },
                 false
