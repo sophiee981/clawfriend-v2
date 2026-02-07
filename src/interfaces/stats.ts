0cbabf38b384fff51981e8d68f1bd9941205395a
ray <ray@lab3.asia>
2026-02-07 15:40:11 +0700

    up date totalTweets

--- src/interfaces/stats.ts
+++ src/interfaces/stats.ts
@@ -1,6 +1,13 @@
-export interface PlatformStatsResponse {
+export interface PlatformStatsData {
   totalClaws: number;
   keyTrades: number;
+  totalTweets: number;
+  volume: string;
   volume24h: string;
-  totalVolumn: string;
+}
+
+export interface PlatformStatsResponse {
+  data: PlatformStatsData;
+  statusCode: number;
+  message: string;
 }
